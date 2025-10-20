03b897ccccdae6141b43efdaba2829af3bfb5c7d
Dlyn1705 <ldo17052005@gmail.com>
2025-10-20 09:00:15 +0700

    Thử branch  mới

--- BTNhom_LapTrinhMang_UngDungFTP/FTP.Server/Forms/FtpServerForm.Designer.cs
+++ BTNhom_LapTrinhMang_UngDungFTP/FTP.Server/Forms/FtpServerForm.Designer.cs
@@ -29,10 +29,17 @@ namespace FTP.Server
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FtpServerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "FtpServerForm";
+            this.Text = "FTP Server";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
